3701360a59b5044fefcabba02fd7e7b851139e82
Francisco Vaz <ftomasvaz@gmail.com>
2020-07-04 23:47:59 +0100

    get name from intro page

--- src/pages/Intro/index.tsx
+++ src/pages/Intro/index.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useCallback, useState, ChangeEvent } from 'react';
+import { Container } from './styles';
 
 const Intro: React.FC = () => {
-  return <h1>Whats your name</h1>;
+  const [name, setName] = useState('');
+
+  const handleNameChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    setName(e.target.value);
+  }, []);
+
+  const handleGoIn = useCallback(() => {
+    console.log(name);
+  }, [name]);
+
+  return (
+    <Container>
+      {/* <h1>Whats your name?</h1> */}
+      <input
+        type="text"
+        placeholder="Whats your name?"
+        onChange={handleNameChange}
+      />
+      <button type="button" onClick={handleGoIn}>
+        Entrar
+      </button>
+    </Container>
+  );
 };
 
 export default Intro;
