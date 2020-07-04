b18d434453f1740eec671a3b3d974e02c3ca85cd
Francisco Vaz <ftomasvaz@gmail.com>
2020-07-04 23:20:51 +0100

    add routes and page home

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 
 import Intro from './pages/Intro';
+import Home from './pages/Home';
 
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
       <Route path="/" exact component={Intro} />
+      <Route path="/home" component={Home} />
     </BrowserRouter>
   );
 };
