22d2cadec9c0d92ff195e51f428ae5b7148299cb
Francisco Vaz <ftomasvaz@gmail.com>
2020-07-04 23:26:30 +0100

    add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import Routes from './routes';
+import GloablStyles from './styles/global';
 
 const App: React.FC = () => {
-  return <Routes />;
+  return (
+    <>
+      <Routes />
+      <GloablStyles />
+    </>
+  );
 };
 
 export default App;
