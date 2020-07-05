ec2820f4491c9131f5a95de82f002df305c80efe
Francisco Vaz <ftomasvaz@gmail.com>
2020-07-06 00:18:59 +0100

    remove user name context from home component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import { Container } from './styles';
 
-import { useUserName } from '../../hooks/userName';
-
 import Header from '../../components/Header';
 import Photo from '../../components/Photo';
 
 const Home: React.FC = () => {
-  const { name } = useUserName();
-
   return (
     <>
       <Container>
