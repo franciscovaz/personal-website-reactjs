7096aba660782c291a38522eb86f72b0009bf36e
Francisco Vaz <ftomasvaz@gmail.com>
2020-07-06 01:03:23 +0100

    added particles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Particles from 'react-particles-js';
 import Routes from './routes';
 import GloablStyles from './styles/global';
 
@@ -7,10 +8,42 @@ import { UserNameProvider } from './hooks/userName';
 
 const App: React.FC = () => {
   return (
-    <UserNameProvider>
-      <Routes />
-      <GloablStyles />
-    </UserNameProvider>
+    <>
+      <UserNameProvider>
+        <Routes />
+        <GloablStyles />
+      </UserNameProvider>
+      <Particles
+        /* style={{position: "fixed",
+        width: '100%',
+        height: '100%',
+        z-index: '10',
+        pointer-events: 'none'}} */
+        params={{
+          particles: {
+            number: {
+              value: 50,
+            },
+            size: {
+              value: 3,
+            },
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: true,
+                mode: 'repulse',
+              },
+            },
+          },
+          background: {
+            color: {
+              value: '#000',
+            },
+          },
+        }}
+      />
+    </>
   );
 };
 
