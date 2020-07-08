1dda850b995a0b2391825503cc914541c8bead42
Francisco Vaz <ftomasvaz@gmail.com>
2020-07-08 14:58:54 +0100

    add css grid to cards

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -12,6 +12,35 @@ const Skills: React.FC = () => {
         <CardContainer>
           <img
             src="https://img.favpng.com/4/1/17/react-javascript-vue-js-logo-png-favpng-T97hHj5T2UsnURsbZ0PB5Mi3c.jpg"
+            width={150}
+            alt="React Skill"
+          />
+          <h1>ReactJS</h1>
+          <p>
+            I have a passion for React since i was started developing with the
+            framework. With a background in angular it was better to understand
+            React faster
+          </p>
+        </CardContainer>
+
+        <CardContainer>
+          <img
+            src="https://img.favpng.com/4/1/17/react-javascript-vue-js-logo-png-favpng-T97hHj5T2UsnURsbZ0PB5Mi3c.jpg"
+            width={150}
+            alt="React Skill"
+          />
+          <h1>ReactJS</h1>
+          <p>
+            I have a passion for React since i was started developing with the
+            framework. With a background in angular it was better to understand
+            React faster
+          </p>
+        </CardContainer>
+
+        <CardContainer>
+          <img
+            src="https://img.favpng.com/4/1/17/react-javascript-vue-js-logo-png-favpng-T97hHj5T2UsnURsbZ0PB5Mi3c.jpg"
+            width={150}
             alt="React Skill"
           />
           <h1>ReactJS</h1>
--- src/components/Skills/styles.ts
+++ src/components/Skills/styles.ts
@@ -1,14 +1,40 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
-  display: flex;
+  /* display: flex;
   flex-direction: column;
-  /* height: 100vh; */
+
   justify-content: center;
-  align-items: center;
+  align-items: center; */
+  display: grid;
+  grid-template-columns: minmax(300px, 1fr);
+  justify-content: center;
+  grid-gap: 2rem;
+  margin: 2rem;
 `;
 
-export const CardContainer = styled.div``;
+export const CardContainer = styled.div`
+  box-shadow: 0px 2px 8px 0px #ccc;
+  text-align: center;
+  border-radius: 16px;
+  position: relative;
+  overflow: hidden;
+
+  img {
+    width: 8rem;
+    clip-path: circle(60px at center);
+    margin-top: 4.5rem;
+  }
+
+  h1 {
+    font-size: 1.5rem;
+  }
+
+  p {
+    margin: 1rem 2rem;
+    font-size: 0.9rem;
+  }
+`;
 
 export const SectionTitle = styled.div`
   text-align: center;
