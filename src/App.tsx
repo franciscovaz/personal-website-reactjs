ee5381f89bf4a795775f009ae4cf77d660b7c7d1
Francisco Vaz <ftomasvaz@gmail.com>
2020-07-11 21:24:00 +0100

    create toggleTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { DefaultTheme, ThemeProvider } from 'styled-components';
 import Routes from './routes';
 import GloablStyles from './styles/global';
+
+import dark from './styles/themes/dark';
+import light from './styles/themes/light';
+import SwitchComponent from './components/Switch';
+
+import usePersistedState from './utils/usePersistedState';
 
 import { UserNameProvider } from './hooks/userName';
 
 const App: React.FC = () => {
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', dark);
+
+  const toggleTheme = useCallback(() => {
+    setTheme(theme.title === 'dark' ? light : dark);
+  }, [setTheme, theme.title]);
+
   return (
     <>
-      <UserNameProvider>
-        <Routes />
-        <GloablStyles />
-      </UserNameProvider>
+      <ThemeProvider theme={theme}>
+        <UserNameProvider>
+          <Routes />
+          <GloablStyles />
+        </UserNameProvider>
+        <SwitchComponent toggleTheme={toggleTheme} />
+      </ThemeProvider>
     </>
   );
 };
