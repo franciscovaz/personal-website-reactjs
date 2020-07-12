0f29d5c2ef66c4bbd24a3032913d1344e06b0932
Francisco Vaz <ftomasvaz@gmail.com>
2020-07-12 18:59:50 +0100

    add particles to home and intro

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Particles from 'react-tsparticles';
+import { ThemeContext } from 'styled-components';
 import { Container } from './styles';
-
 import Header from '../../components/Header';
 import Photo from '../../components/Photo';
 import About from '../../components/About';
@@ -11,9 +12,87 @@ import Contact from '../../components/Contact';
 import Footer from '../../components/Footer';
 
 const Home: React.FC = () => {
+  const { colors } = useContext(ThemeContext);
   return (
     <>
       <Container>
+        <Particles
+          params={{
+            background: {
+              color: {
+                value: colors.primary,
+              },
+            },
+            fpsLimit: 60,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: 'push',
+                },
+                onHover: {
+                  enable: true,
+                  mode: 'repulse',
+                },
+                resize: true,
+              },
+              modes: {
+                bubble: {
+                  distance: 400,
+                  duration: 2,
+                  opacity: 0.8,
+                  size: 40,
+                },
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: colors.secundary,
+              },
+              links: {
+                color: colors.secundary,
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+              },
+              collisions: {
+                enable: true,
+              },
+              move: {
+                enable: true,
+                random: false,
+                speed: 6,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  value_area: 800,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: 'circle',
+              },
+              size: {
+                random: true,
+                value: 5,
+              },
+            },
+            detectRetina: true,
+          }}
+        />
         <Header />
         <Photo />
         <Projects />
--- src/pages/Intro/index.tsx
+++ src/pages/Intro/index.tsx
@@ -1,6 +1,8 @@
-import React, { useCallback, useState, ChangeEvent } from 'react';
+import React, { useCallback, useState, ChangeEvent, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+import Particles from 'react-tsparticles';
+import { ThemeContext } from 'styled-components';
 import { Container, Input } from './styles';
 
 import { useUserName } from '../../hooks/userName';
@@ -10,6 +12,7 @@ const Intro: React.FC = () => {
   const { setUserName } = useUserName();
   const [hasError, setHasError] = useState(false);
   const history = useHistory();
+  const { colors } = useContext(ThemeContext);
 
   const handleNameChange = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => {
@@ -46,6 +49,83 @@ const Intro: React.FC = () => {
       <button type="button" onClick={handleGoToWebsite}>
         <FiLogIn size={24} color="#ffcc13" />
       </button>
+      <Particles
+        params={{
+          background: {
+            color: {
+              value: colors.primary,
+            },
+          },
+          fpsLimit: 60,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: 'push',
+              },
+              onHover: {
+                enable: true,
+                mode: 'repulse',
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: colors.secundary,
+            },
+            links: {
+              color: colors.secundary,
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              enable: true,
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: 'circle',
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
     </Container>
   );
 };
