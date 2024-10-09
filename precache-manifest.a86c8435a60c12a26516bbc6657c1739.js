1ccdd13d672ebda7ec4c826c778912e4d7a5ad82
franciscovaz <ftomasvaz@gmail.com>
2024-10-09 19:50:30 +0100

    Updates

---
+++ precache-manifest.a86c8435a60c12a26516bbc6657c1739.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd02d0b23458bab7644ac2d8e825c7e8",
+    "url": "/personal-website-reactjs/index.html"
+  },
+  {
+    "revision": "dc2ce49d3965f5425de1",
+    "url": "/personal-website-reactjs/static/js/2.f3cd8744.chunk.js"
+  },
+  {
+    "revision": "6e9429c2db6f5fd481230796f6252394",
+    "url": "/personal-website-reactjs/static/js/2.f3cd8744.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f57c1d4069133ec272cc",
+    "url": "/personal-website-reactjs/static/js/main.8a1cde73.chunk.js"
+  },
+  {
+    "revision": "5010f1cab7e5ea814c3b",
+    "url": "/personal-website-reactjs/static/js/runtime-main.9eeca694.js"
+  },
+  {
+    "revision": "db6caebe74e3650305058c9d5f64058e",
+    "url": "/personal-website-reactjs/static/media/perfil1.db6caebe.jpg"
+  }
+]);
